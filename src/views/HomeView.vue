aa4d6d57ac06142461e380540f6d41365746ce8d
çağrı karayaka <cagri.karayaka@gmail.com>
2022-05-13 21:12:38 +0300

    cmd

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    
+    <hello-world msg="Welcome to Your Vue.js App"/>
   </div>
 </template>
 
@@ -10,6 +10,7 @@
 import HelloWorld from '@/components/HelloWorld.vue'
 
 export default {
+
   name: 'HomeView',
   components: {
     HelloWorld
