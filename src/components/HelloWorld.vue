7bd017e6ebb54b6d9120558173564e3d450ec2e6
çağrı karayaka <cagri.karayaka@gmail.com>
2022-05-13 21:13:34 +0300

    smms

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -16,7 +16,7 @@
         </h1>
 
         <p class="subheading font-weight-regular">
-          For help and collaboration with other Vuetify developers,
+         Helleo word değişti
           <br>please join our online
           <a
             href="https://community.vuetifyjs.com"
